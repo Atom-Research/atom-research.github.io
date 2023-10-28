08eb58540528fc36863adbdc3b1d898e2d5efce3
palmtrey <52005788+palmtrey@users.noreply.github.com>
2023-10-28 19:43:02 -0400

    Add code from GPT4

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Adding a simple script to handle form submission
+document.querySelector('#contact form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Collect form data and send it to your server
+    // ...
+});
