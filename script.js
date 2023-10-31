e15ef28babc67448a9a22d9a01b00ed108637ab4
palmtrey <52005788+palmtrey@users.noreply.github.com>
2023-10-31 14:27:42 -0400

    First export simply static

--- script.js
+++
@@ -1,7 +0,0 @@
-// Example: Adding a simple script to handle form submission
-document.querySelector('#contact form').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // Collect form data and send it to your server
-    // ...
-});
